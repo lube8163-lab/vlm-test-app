c7a1736c641b81003bfce95f08fb3fb698ab40b3
Tasuku Kato <lube8163@icloud.com>
2026-03-03 22:09:01 +0900

    Initial Commit

---
+++ vlm_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vlm_test
+//
+//  Created by Tasuku Kato on 2026/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vlm_test/vlm_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  vlm_testApp.swift
+//  vlm_test
+//
+//  Created by Tasuku Kato on 2026/03/03.
+//
+
+import SwiftUI
+
+@main
+struct vlm_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
